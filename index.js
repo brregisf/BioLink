0ebc7d0013f1060c53b608fe711796f57793b199
brregisf <lucasregisf@icloud.com>
2024-09-02 05:53:17 -0300

    1.20.01

---
+++ index.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal(".media a", {distance: '20px', interval: 200})
+
+ScrollReveal().reveal(".content-desc h2", {distance: '20px', origin: 'left'})
+ScrollReveal().reveal(".paragrafo", {distance: '20px'})
+
+ScrollReveal().reveal(".content-2 h2", {distance: '20px', origin: 'left'})
+ScrollReveal().reveal(".tecnos", {distance: '20px'})
+
+ScrollReveal().reveal(".content-3 h2", {distance: '20px', origin: 'left'})
+ScrollReveal().reveal(".box", {distance: '20px'})
+ScrollReveal().reveal(".footer", {distance: '20px'})
